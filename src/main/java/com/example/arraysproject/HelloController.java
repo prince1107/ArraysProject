1ed3c74168578741bc7785b9826e9fb576adf3f8
prince1107 <princeofwakanda0607@gmail.com>
2022-09-22 19:19:18 -0500

    Project 1, 2, done, 4 in progress

--- src/main/java/com/example/arraysproject/HelloController.java
+++ src/main/java/com/example/arraysproject/HelloController.java
@@ -1,14 +1,126 @@
 package com.example.arraysproject;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.math.*;
+import java.util.Arrays;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15;
+
+    @FXML
+    private TextField input1, input2;
+
+    @FXML
+    private Label resultLabel;
+
+    @FXML
+    public void onHelloButtonClick() {
+        findNumInArray();
+        arrayMaxMinAvg();
+        rollXDiceYTimes();
+    }
+
+    @FXML
+    public void findNumInArray(){
+        int num = 5;
+        int[] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int[] array2 = {1,4,2,3,5,5,3,80,1,2,3,5,3,4,5,6,7,4,3,6,20,78,45};
+        String positions = "Positions: ";
+        int count = 0;
+
+        for (int i = 0; i < array2.length; i++){
+            if (array2[i] == num){
+                positions += i + ", ";
+                count++;
+            }
+        }
+        positions += "How Many: " + count;
+
+        System.out.println(positions);
+    }
+
+    @FXML
+    public void arrayMaxMinAvg(){
+        int[] array = new int[(int) (Math.random()*100)];
+        String result = "";
+
+        for (int i = 0; i < array.length; i++){
+            array[i] = (int) (Math.random()*1000);
+        }
+
+//        for testing vvvv
+//        result += "Array: " + Arrays.toString(array) + "; ";
+        result += "Max: " + findMax(array) + "; ";
+        result += "Min: " + findMin(array) + "; ";
+        result += "Average: " + findAverage(array);
+
+        System.out.println(result);
+    }
+
+    protected int findMax(int[] tempArray){
+        int max = 0;
+
+        for (int element:tempArray) {
+            if (element > max){
+                max = element;
+            }
+        }
+        return max;
+    }
+
+    protected int findMin(int[] tempArray){
+        int min = 500000;
+
+        for (int element:tempArray) {
+            if (element < min){
+                min = element;
+            }
+        }
+        return min;
+    }
+
+    protected double findAverage(int[] tempArray){
+        double sum = Arrays.stream(tempArray).sum();
+        double length = tempArray.length;
+        double average = sum/length;
+
+        return average;
+    }
+
+    @FXML
+    public void mutateArray(){
+
+    }
+
+    @FXML
+    protected void rollXDiceYTimes(){
+        int x = 2;
+        int y = 20;
+
+        int numFrequencies = x*6 - (x-1);
+        int[] frequencies = new int[numFrequencies];
+        int temp;
+
+        String result = "";
+
+        for (int i = 0; i < y; i++){
+            temp = 0;
+            for (int j = 0; j < x; j++){
+                temp += (int) Math.round(Math.random()*5) + 1;
+            }
+            frequencies[temp - (x-1)]++;
+        }
+
+        result += "Array: " + Arrays.toString(frequencies) + "; ";
+
+        System.out.println(result);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.arraysproject {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.arraysproject to javafx.fxml;
     exports com.example.arraysproject;
 }
